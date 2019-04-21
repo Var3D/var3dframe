f0c0a3d5783f7af1d1a248c6725e9f3665f7b730
Var3D <aijiuziji@126.com>
2019-04-21 19:19:23 +0800

    为了安卓混淆,现在setResources方法恢复

--- core/src/core/java/var3d/net/center/DialogMessge.java
+++ core/src/core/java/var3d/net/center/DialogMessge.java
@@ -8,8 +8,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 
-import static var3d.net.center.VDialog.ActionType.MOVEUP;
-
 
 /**
  * Created by fengyu on 2017/10/24.
