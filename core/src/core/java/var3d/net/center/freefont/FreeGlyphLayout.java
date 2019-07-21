2e07cd1c6c9909dbdfb5f95d39a4853263ce4dd6
fengyu <aijiuziji@126.com>
2019-07-21 17:35:59 +0800

    VTextField输入 emoji 时保持 emoji的原色， 不被输入框的文本颜色设置影响，移出一些无用代码

--- core/src/core/java/var3d/net/center/freefont/FreeGlyphLayout.java
+++ core/src/core/java/var3d/net/center/freefont/FreeGlyphLayout.java
@@ -1,16 +1,13 @@
 package var3d.net.center.freefont;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Colors;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.FloatArray;
 import com.badlogic.gdx.utils.Pool;
 import com.badlogic.gdx.utils.Pools;
-import com.badlogic.gdx.utils.StringBuilder;
 
 /**
  * Created by fengyu on 2019/7/20.
@@ -18,27 +15,11 @@ import com.badlogic.gdx.utils.StringBuilder;
 
 public class FreeGlyphLayout extends GlyphLayout {
     private final Array<Color> colorStack = new Array(4);
-    private StringBuffer buffer;
     private Color inColor;
 
     public void setText (BitmapFont font, CharSequence str, int start, int end, Color color, float targetWidth, int halign,
                          boolean wrap, String truncate) {
         FreeBitmapFont freefont= (FreeBitmapFont) font;
-        if(freefont.isEmoji()){
-            if(buffer==null)buffer=new StringBuffer();
-            buffer.setLength(0);
-
-            for(int i=0;i<str.length();i++){
-                char c=str.charAt(i);
-                if(freefont.isCreatedEmoji4WithKey(""+c)||freefont.isCreatedEmoji2WithKey(""+c)){
-                    //buffer.append("[白]");
-                    buffer.append(c);
-                    //end+=3;
-                }else buffer.append(c);
-            }
-            str=buffer.toString();
-        }
-
 
         inColor=color;
         int intStart=start;
@@ -56,15 +37,7 @@ public class FreeGlyphLayout extends GlyphLayout {
 
         Array<Color> colorStack = this.colorStack;
         Color nextColor = null;
-  //      String ts = str.subSequence(start, start+1).toString();
-//        boolean isFirstIsEmoji;//第一个字符为 emoji
-//        if(!freefont.isEmoji()||!freefont.isCreateEmojiWithKey(ts)) {
-//            nextColor= inColor;
-//            isFirstIsEmoji=false;
-//        }else{
-//            nextColor=Color.WHITE;
-//            isFirstIsEmoji=true;
-//        }
+
         colorStack.add(nextColor);
 
         Pool<Color> colorPool = Pools.get(Color.class);
@@ -272,106 +245,6 @@ public class FreeGlyphLayout extends GlyphLayout {
             return i - start;
         }
         return -1; // Unclosed color tag.
-    }
-
-    private void truncate (BitmapFont.BitmapFontData fontData, GlyphRun run, float targetWidth, String truncate, int widthIndex,
-                           Pool<GlyphRun> glyphRunPool) {
-
-        // Determine truncate string size.
-        GlyphRun truncateRun = glyphRunPool.obtain();
-        fontData.getGlyphs(truncateRun, truncate, 0, truncate.length(), true);
-        float truncateWidth = 0;
-        for (int i = 1, n = truncateRun.xAdvances.size; i < n; i++)
-            truncateWidth += truncateRun.xAdvances.get(i);
-        targetWidth -= truncateWidth;
-
-        // Determine visible glyphs.
-        int count = 0;
-        float width = run.x;
-        while (count < run.xAdvances.size) {
-            float xAdvance = run.xAdvances.get(count);
-            width += xAdvance;
-            if (width > targetWidth) {
-                run.width = width - run.x - xAdvance;
-                break;
-            }
-            count++;
-        }
-
-        if (count > 1) {
-            // Some run glyphs fit, append truncate glyphs.
-            run.glyphs.truncate(count - 1);
-            run.xAdvances.truncate(count);
-            adjustLastGlyph(fontData, run);
-            if (truncateRun.xAdvances.size > 0) run.xAdvances.addAll(truncateRun.xAdvances, 1, truncateRun.xAdvances.size - 1);
-        } else {
-            // No run glyphs fit, use only truncate glyphs.
-            run.glyphs.clear();
-            run.xAdvances.clear();
-            run.xAdvances.addAll(truncateRun.xAdvances);
-            if (truncateRun.xAdvances.size > 0) run.width += truncateRun.xAdvances.get(0);
-        }
-        run.glyphs.addAll(truncateRun.glyphs);
-        run.width += truncateWidth;
-
-        glyphRunPool.free(truncateRun);
-    }
-
-    private GlyphRun wrap (BitmapFont.BitmapFontData fontData, GlyphRun first, Pool<GlyphRun> glyphRunPool, int wrapIndex, int widthIndex) {
-        GlyphRun second = glyphRunPool.obtain();
-        second.color.set(first.color);
-        int glyphCount = first.glyphs.size;
-
-        // Increase first run width up to the end index.
-        while (widthIndex < wrapIndex)
-            first.width += first.xAdvances.get(widthIndex++);
-
-        // Reduce first run width by the wrapped glyphs that have contributed to the width.
-        while (widthIndex > wrapIndex + 1)
-            first.width -= first.xAdvances.get(--widthIndex);
-
-        // Copy wrapped glyphs and xAdvances to second run.
-        // The second run will contain the remaining glyph data, so swap instances rather than copying to reduce large allocations.
-        if (wrapIndex < glyphCount) {
-            Array<BitmapFont.Glyph> glyphs1 = second.glyphs; // Starts empty.
-            Array<BitmapFont.Glyph> glyphs2 = first.glyphs; // Starts with all the glyphs.
-            glyphs1.addAll(glyphs2, 0, wrapIndex);
-            glyphs2.removeRange(0, wrapIndex - 1);
-            first.glyphs = glyphs1;
-            second.glyphs = glyphs2;
-            // Equivalent to:
-            // second.glyphs.addAll(first.glyphs, wrapIndex, glyphCount - wrapIndex);
-            // first.glyphs.truncate(wrapIndex);
-
-            FloatArray xAdvances1 = second.xAdvances; // Starts empty.
-            FloatArray xAdvances2 = first.xAdvances; // Starts with all the xAdvances.
-            xAdvances1.addAll(xAdvances2, 0, wrapIndex + 1);
-            xAdvances2.removeRange(1, wrapIndex); // Leave first entry to be overwritten by next line.
-            xAdvances2.set(0, -glyphs2.first().xoffset * fontData.scaleX - fontData.padLeft);
-            first.xAdvances = xAdvances1;
-            second.xAdvances = xAdvances2;
-            // Equivalent to:
-            // second.xAdvances.add(-second.glyphs.first().xoffset * fontData.scaleX - fontData.padLeft);
-            // second.xAdvances.addAll(first.xAdvances, wrapIndex + 1, first.xAdvances.size - (wrapIndex + 1));
-            // first.xAdvances.truncate(wrapIndex + 1);
-        }
-
-        if (wrapIndex == 0) {
-            // If the first run is now empty, remove it.
-            glyphRunPool.free(first);
-            runs.pop();
-        } else
-            adjustLastGlyph(fontData, first);
-
-        return second;
-    }
-
-    private void adjustLastGlyph (BitmapFont.BitmapFontData fontData, GlyphRun run) {
-        BitmapFont.Glyph last = run.glyphs.peek();
-        if (fontData.isWhitespace((char)last.id)) return; // Can happen when doing truncate.
-        float width = (last.xoffset + last.width) * fontData.scaleX - fontData.padRight;
-        run.width += width - run.xAdvances.peek(); // Can cause the run width to be > targetWidth, but the problem is minimal.
-        run.xAdvances.set(run.xAdvances.size - 1, width);
     }
 
 }
