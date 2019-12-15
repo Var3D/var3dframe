0f4fe2dfd4261ee255f7fdc2400f2076dec5f9cd
yufeng <aijiuziji@126.com>
2019-12-16 00:55:57 +0800

    DialogLoad增加关闭按钮

--- core/src/core/java/var3d/net/center/component/DialogLoad.java
+++ core/src/core/java/var3d/net/center/component/DialogLoad.java
@@ -5,9 +5,14 @@ package var3d.net.center.component;
  */
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Array;
 
@@ -29,17 +34,39 @@ public class DialogLoad extends VDialog {
 
     @Override
     public void init() {
-        setBackground(30,30, Color.CLEAR);
-        group=game.getGroup(getWidth(),getHeight()).touchOff().setOrigin(Align.center).setPosition(
+        setBackground(Color.CLEAR);
+        group=game.getGroup(30,30).touchOff().setOrigin(Align.center).setPosition(
                 getWidth()*0.5f,getHeight()*0.5f,Align.center).show();
         for(int i=0;i<12;i++){
-            Image img_line=game.getImage(group.getWidth(),8).setPosition(getWidth()*0.5f+25
-                    ,getHeight()*0.5f,Align.left).touchOff().setOrigin(-25,self()
+            Image img_line=game.getImage(group.getWidth(),8).setPosition(group.getWidth()*0.5f+25
+                    ,group.getHeight()*0.5f,Align.left).touchOff().setOrigin(-25,self()
                     .getHeight()*0.5f).setAlpha(1-0.076f*i).show(group);
             img_line.setRotation(i*30);
             lines.add(img_line);
         }
         group.addAction(Actions.forever(Actions.rotateBy(-360,2)));
+
+        int size= (int) (game.getDefaultFontSize()*1.5f);
+        Button img_close=game.getUI(new Button(new TextureRegionDrawable(new TextureRegion(game
+                .getCircleColorTexture(size,Color.valueOf("d70015")))))).addClicAction()
+                .setSize(size,size).setPosition(getWidth()-20,getHeight()-20,Align.topRight).show();
+        Image line_x=game.getImage(size*0.7f,6).setOrigin(Align.center).touchOff().setPosition(size*0.5f
+                ,size*0.5f,Align.center).show(img_close);
+        line_x.setRotation(45);
+        Image line_Y=game.getImage(6,size*0.7f).setOrigin(Align.center).touchOff().setPosition(size*0.5f
+                ,size*0.5f,Align.center).show(img_close);
+        line_Y.setRotation(45);
+        img_close.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                if(run!=null)run.run();
+                game.removeDialog();
+            }
+        });
+    }
+
+    private Runnable run;
+    public void setCloseRunnable(Runnable run){
+        this.run=run;
     }
 
     @Override
@@ -49,6 +76,7 @@ public class DialogLoad extends VDialog {
 
     @Override
     public void show() {
+
     }
 
     @Override
--- core/src/main/java/var3d/net/demo/stages/StageMain.java
+++ core/src/main/java/var3d/net/demo/stages/StageMain.java
@@ -145,14 +145,14 @@ public class StageMain extends VStage {
 
 
         //文本测试
-        VLabel msg = game.getLabel("[红]红[橙],,橙[黄]黄[绿]绿[青]青,[蓝]蓝[紫]紫[橄榄色]橄榄色[品红]品红[海蓝]海蓝[天蓝],,天蓝[砖红色]砖红色[金]金[屎黄]屎黄").show(this);
+        VLabel msg = game.getLabel("我是😯一个粉刷😊匠，粉刷️本领强").show(this);
+        msg.setText("[黑]我是😯一个粉刷😊匠，粉刷️本领强");
         msg.setDebug(true);
         msg.setAlignment(Align.center, Align.center);
         msg.setWidth(getWidth()*0.4f);
         msg.setWrap(true);
         msg.setPosition(getWidth() * 0.5f, getHeight() * 0.5f, Align.center);
 
-        game.showDialog(DialogLoad.class);
     }
     public void start() {
     }
